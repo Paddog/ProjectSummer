822fb4f9671df08ba738143aa356413457a2f8c0
iBac0n <leon.mittelmeyer@outlook.de>
2017-07-19 19:37:46 +0200

    NetworkBasics
    Position Sync
    Coruch Sync

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 
 
@@ -13,7 +14,7 @@ public enum CharacterStates {
 
 //CRITICAL: Synchronization on the network
 [RequireComponent(typeof(Rigidbody2D))]
-public class CharController : MonoBehaviour {
+public class CharController : NetworkBehaviour {
 
     public float speed;
     public float jumpPower;
@@ -26,28 +27,37 @@ public class CharController : MonoBehaviour {
     private Rigidbody2D rb;
     private CharacterStates cs;
 
+    private bool isDuck = false;
 
-	void Start () {
+
+    private float cooldown = 1;
+    private float timestamp = 0;
+
+    void Start () {
         rb = this.GetComponent<Rigidbody2D>();
         cs = CharacterStates.normal;
 	}
 	
 
     /*
-     * Vectoren 2d
-     * Character muss sich bewegen links rechts springen ducken /// klettern
+     * 
+     * Character muss sich bewegen links rechts springen ducken x /// klettern
      * 
      * 
      * 
      * 
      * 
      * Character animation,
-     * Character states isDucken true, isJumpen true, isBewegung true, isInteragieren true,
+     * Character states isDucken true, isJumpen true, isBewegung true, x         isInteragieren true, 
      * 
     */
 
 
 	void Update () {
+
+        if (!isLocalPlayer) { return; }
+
+
         //springen und ground check
         grounded = Physics2D.OverlapCircle(groundCheck.position, radius, groundLayer);
 
@@ -55,7 +65,7 @@ public class CharController : MonoBehaviour {
         {
             cs = CharacterStates.springen;
         }
-        else {
+        else if(grounded == true && isDuck == false) {
             cs = CharacterStates.normal;
         }
 
@@ -65,11 +75,45 @@ public class CharController : MonoBehaviour {
         }
         //TODO: Ducken
         
+        if (Input.GetKeyDown(KeyCode.C) && timestamp <= Time.time)
+        {
+            timestamp = Time.time + cooldown;
+            isDuck = !isDuck;
+            CmdCrouchToggle(isDuck);
+        }
 
     }
 
-    void FixedUpdate(){
+    [Command]
+    public void CmdCrouchToggle(bool id) {
+        Debug.LogError("Server: CrouchToggle()");
+        BoxCollider2D bc2d = this.GetComponent<BoxCollider2D>();
+        if (id == true)
+        {
+            //CRITICAL: Isnt relative to size, be careful if you have another size then 4!
+            bc2d.size = new Vector2(bc2d.size.x, bc2d.size.y / 2);
+            bc2d.offset = new Vector2(bc2d.offset.x, (bc2d.size.y - 1) / -1);
+            cs = CharacterStates.ducken;
+        }
+        else
+        {
+            bc2d.size = new Vector2(bc2d.size.x, bc2d.size.y * 2);
+            bc2d.offset = new Vector2(0, 0);
+        }
+        Debug.LogError("Sending info to client!");
+        RpcSendColliderInfo(bc2d.size.x, bc2d.size.y, bc2d.offset.x, bc2d.offset.y);
+    }
 
+    [ClientRpc]
+    public void RpcSendColliderInfo(float x, float y, float offsetx, float offsety) {
+        Debug.LogError("Recieving Info!");
+        this.GetComponent<BoxCollider2D>().size = new Vector2(x, y);
+        this.GetComponent<BoxCollider2D>().offset = new Vector2(offsetx, offsety);
+    }
+
+
+    void FixedUpdate(){
+        if (!isLocalPlayer) { return; }
         //Bewegung Horizontal
         float x = 0;
         float y = 0;
