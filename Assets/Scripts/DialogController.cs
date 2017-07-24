85be9522b138b29494cfb8f0f9c0b65c24795cb8
iBac0n <leon.mittelmeyer@outlook.de>
2017-07-24 12:08:03 +0200

    Dialog Controller + KeyLock Scripts

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogController : MonoBehaviour {
+    
+    public List<GameObject> uiElements = new List<GameObject>();
+
+
+    void Start() {
+
+    }
+
+    //CharController->ShowDialog("KeyLock")->Zeigt Dialog KeyLock an-> 
+    //Bitte Zeige den Dialog mit dem string(namen) "KeyLock" an.
+    //Kontrolliere die Interaktion zwischen Ui und Spieler
+
+    public void ShowDialog(string name) {
+        foreach(GameObject element in uiElements)
+        {
+            if(element.name == name)
+            {
+                element.SetActive(true);
+            }
+        }
+    }
+    public void HideDialog(string name)
+    {
+        foreach(GameObject element in uiElements)
+        {
+            if(element.name == name)
+            {
+                element.SetActive(false);
+            }
+        }
+    }
+
+    public GameObject GetUiElement(string name) {
+        foreach (GameObject element in uiElements) {
+            if (element.name == name)
+            {
+                return element;
+            }
+            else {
+                return null;
+            }
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/KeyLock.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+
+//CRITICAL: There will be 2 CodeLocks For 1 Door, The 2 coresponding CodeLocks have to be the same state and need to have the same code
+public class KeyLock : NetworkBehaviour {
+
+    public GameObject connectedDoor;
+    public Door conDoorScript;
+    public bool isCodeKeyLock;
+
+
+
+    [SyncVar]
+    public int code;
+    [SyncVar(hook = "ChangeIsLockedState")]
+    public bool isLocked;
+
+    
+
+	void Start () {
+        conDoorScript = connectedDoor.GetComponent<Door>();
+        if (isServer) {
+            //TODO: Set Code(Predefine Puzzles)
+            code = 1337;
+            isLocked = true;
+            conDoorScript.isLocked = true;
+        }
+	}
+	
+
+    private void ChangeIsLockedState(bool _isLocked) {
+        if (_isLocked)
+        {
+            conDoorScript.isLocked = true;
+        }
+        else {
+            conDoorScript.isLocked = false;
+        }
+        isLocked = _isLocked;
+        
+    }
+
+    public bool EnterCode(int _code) {
+        if (_code == code)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            collision.GetComponent<CharController>().interactableGO = this.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            collision.GetComponent<CharController>().interactableGO = null;
+        }
+    }
+
+
+}
