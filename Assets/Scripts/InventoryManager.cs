952dd459d3bb16f0d9d0a314263a0826e536a88f
paddog <paddog123@googlemail.com>
2017-08-01 21:30:05 +0200

    Added Visual Reference for Items/Merge Issue
    Merge Issue Last 2 Commits from this Commit are rolled back

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour {
     public List<Item> items = new List<Item>();
+    public List<InventorySlot> inventorySlots = new List<InventorySlot>();
+
+
+    private GameObject toolBar;
+    void Start() {
+        toolBar = GameObject.Find("ToolBar");
+        for(int i = 0; i < toolBar.transform.childCount; i++) {
+            inventorySlots.Add(toolBar.transform.GetChild(i).GetComponent<InventorySlot>());
+        }
+    }
 
     public void AddItem(Item item) {
-        items.Add(item);
+        if(items.Count < 3) {
+            items.Add(item);
+            SyncItemsWithInventory();
+        } else {
+            Debug.LogError("Inventory full!");
+        }
     }
 
     public void RemoveItem(Item item) {
         items.Remove(item);
+        SyncItemsWithInventory();
     }
 
-
-    //TODO: Maybe we need this not sure
-    public void UpdateGraphics() {
-
+    private void SyncItemsWithInventory() {
+        int count = 0;
+        foreach(Item item in items) {
+            inventorySlots[count].curItem = item;
+            count++;
+        }
     }
+
 }
---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour {
+
+    private Item _curItem;
+    public Item curItem {
+        get { return _curItem; }
+        set { _curItem = value;
+            OnCurrentItemChange();
+        }
+    }
+
+
+    private Image curImage;
+
+    void Start() {
+        GameObject child = this.transform.GetChild(0).gameObject;
+        curImage = child.GetComponent<Image>();
+    }
+   
+
+    public void OnCurrentItemChange() {
+        
+        if(curItem != null) {
+            Sprite loadedImage = Resources.Load(curItem.name, typeof(Sprite)) as Sprite;
+            if(loadedImage != null) {
+                curImage.sprite = loadedImage;
+            } else {
+                Debug.LogError("Sprite for this item is missing: " + curItem.name);
+            }
+
+        } else {
+            Sprite loadedImage = Resources.Load("Default", typeof(Sprite)) as Sprite;
+            if(loadedImage != null) {
+                curImage.sprite = loadedImage;
+            } else {
+                Debug.LogError("Sprite for this item is missing: " + curItem.name);
+            }
+        }
+    }
+
+}
